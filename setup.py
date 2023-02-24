104f6ae7e4b9e55cc44b224b003f3ecde0ac150c
jstzwj <1103870790@qq.com>
2023-02-24 12:37:25 +0800

    update requirements.txt

--- setup.py
+++ setup.py
@@ -192,6 +192,8 @@ for binary data.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     author='Francesc Alted',
     author_email='francesc@blosc.org',
